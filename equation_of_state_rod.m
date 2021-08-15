c8ae8eb17f2a35d0940c133f53b13895ccaddb7c
shimotoriharuki <underbird.h@gmail.com>
2021-08-15 17:45:53 +0900

    たぶん状態方程式できた

--- equation_of_state_rod.m
+++ equation_of_state_rod.m
@@ -1,7 +1,64 @@
 clc
+clf
 
-% ddth1を左辺に持ってくる
-isolate(eqn, diff(th1(t), t, t))
+% parameters
+g = 9.81; % [m/s^2]
+m = 0.1; % [kg]
+l_g = 0.05; % [m]
+I = 1 * 10^-3; % [kg m^2]
+c = 1 * 10^-2.5; % 粘性係数
 
-% th1を左辺に持ってくる
-isolate(eqn, th1)
+X = (g * m * l_g) / (m * l_g^2 + I);
+Y = c / (m * l_g^2 + I);
+Z = 1 / (m * l_g^2 + I);
+
+% equation of state
+A = [0, 1; X, -Y];
+B = [0; -Z];
+C = [1 0];
+
+% 可制御　可観測
+Uc = [B, A*B];
+det(Uc)
+Uo = [C; C*A];
+det(Uo)
+
+% シミュレーション
+
+dt = 0.001;
+t = 0 : dt : 1.5;
+i = 0;
+x = [0.01; 0];
+u = 0.0;
+x1 = [];
+x2 = [];
+for n = t
+    i = i + 1;
+    dx = A * x + B * u;
+    x = x + dx * dt;
+    
+    x1 = [x1, x(1)];
+    x2 = [x2, x(2)];
+end
+
+figure(1)
+plot(t, x1);
+
+% アニメーション
+figure(2)
+xlim([-1 1])
+ylim([-1 1])
+i = 0;
+for n = t
+    i = i + 1;    
+    if rem(i, 1) == 0
+        x = [0 sin(x1(i))];
+        y = [0 cos(x1(i))];
+%         plot([0 sin(x1(i))], [0 cos(x1(i))], '.b'); 
+        g = line(x, y);
+        drawnow;
+        delete(g);
+    end
+end
+
+
