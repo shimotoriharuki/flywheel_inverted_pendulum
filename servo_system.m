268deab5a0d4dcf41fdcf406be533543bfdab9b4
shimotoriharuki <underbird.h@gmail.com>
2021-08-23 22:14:42 +0900

    サーボ系できそう

--- servo_system.m
+++ servo_system.m
@@ -29,49 +29,62 @@ A = [0, 0, 1, 0;
 B = [0; 0; M4; M8];
 C = [1, 1, 0, 0];
 
+Ab = [A, zeros(4, 1);
+      -C, 0];
+Bb = [B; 0];
+
 % 可制御　可観測
-Uc = [B, A*B, A^2*B, A^3*B];
+Uc = [Bb, Ab*Bb, Ab^2*Bb, Ab^3*Bb, Ab^4*Bb];
 if det(Uc) ~= 0
     disp('可制御である')
 end
-Uo = [C; C*A; C*A^2; C*A^3];
+% Uo = [C; C*Ab; C*Ab^2; C*Ab^3; C*Ab^4];
+% 
+% if det(Uo) ~= 0
+%     disp('可観測である')
+% end
 
-if det(Uo) ~= 0
-    disp('可観測である')
-end
 
-% ゲインを求める
-% p = [-1+1i -1-1i -1+1i -1-1i];
-% f = place(A, B, p);
 
-Q = [10, 0, 0, 0;
-     0, 1, 0, 0;
-     0, 0, 0.01, 0;
-     0, 0, 0, 0.01];
+Q = [10, 0, 0, 0, 0;
+     0, 0.001, 0, 0, 0;
+     0, 0, 0.001, 0, 0;
+     0, 0, 0, 0.001, 0;
+     0, 0, 0, 0, 1];
 R = 0.01;
-f = lqr(A, B, Q, R);
+gain = lqr(Ab, Bb, Q, R);
+f = gain(1:4);
+k = -gain(5);
 
 % シミュレーション
 dt = 0.001;
 t = 0 : dt : 10;
-x0 = [pi/2; 0; 0; 0]; % 初期値
+xb0 = [0.1; 0; 0; 0]; % 初期値
+z = 0; % 偏差の積分
+v = [10; 1; 1; 1]; % 外乱
+r = 0; % 目標値
+
+
 u = 0; % 入力の初期値
-x = x0;
+xb = xb0;
 
 s_x1 = [];
 s_x2 = [];
 s_x3 = [];
 s_x4 = [];
+s_z = [];
 s_u = [];
 for n = t
-    dx = A * x + B * u;
-    x = x + dx * dt;
-    u = -f*x;
+    dxb = Ab * [xb; z] + Bb * u + [v; r];
+    xb = xb + dxb(1:4, 1) * dt;
+    z = z + dxb(5) * dt;
+    u = -f*xb + k*z;
     
-    s_x1 = [s_x1 x(1)];
-    s_x2 = [s_x2 x(2)];
-    s_x3 = [s_x3 x(3)];
-    s_x4 = [s_x4 x(4)];
+    s_x1 = [s_x1 xb(1)];
+    s_x2 = [s_x2 xb(2)];
+    s_x3 = [s_x3 xb(3)];
+    s_x4 = [s_x4 xb(4)];
+    s_z = [s_z z];
     s_u = [s_u u];
 
 end
@@ -95,4 +108,14 @@ title('状態フィードバックのシミュレーション')
 subplot(2, 2, 4)
 plot(t, s_x4);
 legend('dth_2')
+title('状態フィードバックのシミュレーション')
+
+figure(4)
+plot(t, s_z);
+legend('z')
+title('状態フィードバックのシミュレーション')
+
+figure(5)
+plot(t, s_u);
+legend('u')
 title('状態フィードバックのシミュレーション')
